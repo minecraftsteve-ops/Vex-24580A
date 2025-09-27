9043d2666f574f38fd48d7053e9bc024d9849ca8
Steven <zhuqicheng873@gmail.com>
2025-09-27 13:42:33 -0700

    s

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ void competition_initialize() {}
  * with the default priority and stack size whenever the robot is enabled via
  * the Field Management System or the VEX Competition Switch in the autonomous
  * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
+ * for non-competition testing purposes. *
  *
  * If the robot is disabled or communications is lost, the autonomous task
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
@@ -92,3 +92,4 @@ void opcontrol() {
 		pros::delay(20);                               // Run for 20 ms then update
 	}
 }
+ 
