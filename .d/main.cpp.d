c7bb156657c981cb44ef63507c11ebf2f094d179
Steven <zhuqicheng873@gmail.com>
2025-09-27 17:16:47 -0700

    yep

--- .d/main.cpp.d
+++ .d/main.cpp.d
@@ -1,9 +1,24 @@
-bin/main.cpp.o: src/main.cpp include/main.h include/api.h \
+bin/main.cpp.o: src/main.cpp include/lemlib/api.hpp \
+ include/lemlib/pid.hpp include/lemlib/pose.hpp include/lemlib/util.hpp \
+ include/lemlib/chassis/chassis.hpp include/pros/rtos.hpp \
+ include/pros/rtos.h include/pros/imu.hpp include/pros/device.hpp \
+ include/pros/misc.hpp include/pros/misc.h include/pros/imu.h \
+ include/lemlib/asset.hpp include/lemlib/chassis/trackingWheel.hpp \
+ include/pros/motors.hpp include/pros/abstract_motor.hpp \
+ include/pros/motors.h include/pros/rtos.hpp include/pros/motor_group.hpp \
+ include/pros/colors.hpp include/pros/adi.hpp include/pros/adi.h \
+ include/pros/rotation.hpp include/pros/rotation.h \
+ include/lemlib/exitcondition.hpp include/lemlib/driveCurve.hpp \
+ include/lemlib/logger/logger.hpp include/fmt/core.h include/fmt/format.h \
+ include/fmt/core.h include/fmt/format-inl.h \
+ include/lemlib/logger/baseSink.hpp include/fmt/args.h \
+ include/lemlib/logger/message.hpp include/lemlib/logger/infoSink.hpp \
+ include/lemlib/logger/telemetrySink.hpp include/main.h include/api.h \
  include/pros/adi.h include/pros/ai_vision.h include/pros/colors.h \
  include/pros/device.h include/pros/distance.h include/pros/error.h \
- include/pros/ext_adi.h include/pros/adi.h include/pros/adi.h \
- include/pros/gps.h include/pros/imu.h include/pros/link.h \
- include/pros/llemu.h include/liblvgl/llemu.h include/liblvgl/lvgl.h \
+ include/pros/ext_adi.h include/pros/adi.h include/pros/gps.h \
+ include/pros/imu.h include/pros/link.h include/pros/llemu.h \
+ include/liblvgl/llemu.h include/liblvgl/lvgl.h \
  include/liblvgl/lv_version.h include/liblvgl/lvgl_private.h \
  include/liblvgl/core/lv_global.h \
  include/liblvgl/core/../lv_conf_internal.h \
@@ -653,19 +668,50 @@ bin/main.cpp.o: src/main.cpp include/main.h include/api.h \
  include/pros/error.h include/pros/rotation.h include/pros/rtos.h \
  include/pros/screen.h include/pros/colors.h include/pros/vision.h \
  include/pros/adi.hpp include/pros/ai_vision.hpp include/pros/ai_vision.h \
- include/pros/device.hpp include/pros/misc.hpp include/pros/misc.h \
- include/pros/rtos.hpp include/pros/rtos.h include/pros/colors.hpp \
- include/pros/device.hpp include/pros/distance.hpp \
- include/pros/distance.h include/pros/gps.hpp include/pros/gps.h \
- include/pros/imu.hpp include/pros/imu.h include/pros/link.hpp \
+ include/pros/colors.hpp include/pros/device.hpp \
+ include/pros/distance.hpp include/pros/distance.h include/pros/gps.hpp \
+ include/pros/gps.h include/pros/imu.hpp include/pros/link.hpp \
  include/pros/link.h include/pros/llemu.hpp include/liblvgl/llemu.hpp \
  include/pros/misc.hpp include/pros/motor_group.hpp \
- include/pros/abstract_motor.hpp include/pros/motors.h \
- include/pros/rtos.hpp include/pros/colors.hpp include/pros/motors.hpp \
  include/pros/motors.hpp include/pros/optical.hpp include/pros/optical.h \
- include/pros/rotation.hpp include/pros/rotation.h include/pros/rtos.hpp \
- include/pros/screen.hpp include/pros/screen.h include/pros/vision.hpp \
- include/pros/vision.h
+ include/pros/rotation.hpp include/pros/rtos.hpp include/pros/screen.hpp \
+ include/pros/screen.h include/pros/vision.hpp include/pros/vision.h
+include/lemlib/api.hpp:
+include/lemlib/pid.hpp:
+include/lemlib/pose.hpp:
+include/lemlib/util.hpp:
+include/lemlib/chassis/chassis.hpp:
+include/pros/rtos.hpp:
+include/pros/rtos.h:
+include/pros/imu.hpp:
+include/pros/device.hpp:
+include/pros/misc.hpp:
+include/pros/misc.h:
+include/pros/imu.h:
+include/lemlib/asset.hpp:
+include/lemlib/chassis/trackingWheel.hpp:
+include/pros/motors.hpp:
+include/pros/abstract_motor.hpp:
+include/pros/motors.h:
+include/pros/rtos.hpp:
+include/pros/motor_group.hpp:
+include/pros/colors.hpp:
+include/pros/adi.hpp:
+include/pros/adi.h:
+include/pros/rotation.hpp:
+include/pros/rotation.h:
+include/lemlib/exitcondition.hpp:
+include/lemlib/driveCurve.hpp:
+include/lemlib/logger/logger.hpp:
+include/fmt/core.h:
+include/fmt/format.h:
+include/fmt/core.h:
+include/fmt/format-inl.h:
+include/lemlib/logger/baseSink.hpp:
+include/fmt/args.h:
+include/lemlib/logger/message.hpp:
+include/lemlib/logger/infoSink.hpp:
+include/lemlib/logger/telemetrySink.hpp:
 include/main.h:
 include/api.h:
 include/pros/adi.h:
@@ -675,7 +721,6 @@ include/pros/device.h:
 include/pros/distance.h:
 include/pros/error.h:
 include/pros/ext_adi.h:
-include/pros/adi.h:
 include/pros/adi.h:
 include/pros/gps.h:
 include/pros/imu.h:
@@ -1370,11 +1415,6 @@ include/pros/vision.h:
 include/pros/adi.hpp:
 include/pros/ai_vision.hpp:
 include/pros/ai_vision.h:
-include/pros/device.hpp:
-include/pros/misc.hpp:
-include/pros/misc.h:
-include/pros/rtos.hpp:
-include/pros/rtos.h:
 include/pros/colors.hpp:
 include/pros/device.hpp:
 include/pros/distance.hpp:
@@ -1382,23 +1422,16 @@ include/pros/distance.h:
 include/pros/gps.hpp:
 include/pros/gps.h:
 include/pros/imu.hpp:
-include/pros/imu.h:
 include/pros/link.hpp:
 include/pros/link.h:
 include/pros/llemu.hpp:
 include/liblvgl/llemu.hpp:
 include/pros/misc.hpp:
 include/pros/motor_group.hpp:
-include/pros/abstract_motor.hpp:
-include/pros/motors.h:
-include/pros/rtos.hpp:
-include/pros/colors.hpp:
-include/pros/motors.hpp:
 include/pros/motors.hpp:
 include/pros/optical.hpp:
 include/pros/optical.h:
 include/pros/rotation.hpp:
-include/pros/rotation.h:
 include/pros/rtos.hpp:
 include/pros/screen.hpp:
 include/pros/screen.h:
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "lemlib/api.hpp" // IWYU pragma: keep
 #include "main.h"
 
 /**
@@ -16,19 +17,78 @@ void on_center_button() {
 	}
 }
 
+pros::MotorGroup left_motor_group({1, 2, -3}); // left motors on ports 1 (reversed), 2 (forwards), and 3 (reversed)
+pros::MotorGroup right_motor_group({-11, -12, 13}); // right motors on ports 4 (forwards), 5 (reversed), and 6 (forwards)
+
+// drivetrain settings
+lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
+	&right_motor_group, // right motor group
+	10, // 10 inch track width
+	lemlib::Omniwheel::NEW_4, // using new 4" omnis
+	360, // drivetrain rpm is 360
+	2 // horizontal drift is 2 (for now)
+);
+
+lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
+	nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+	nullptr, // horizontal tracking wheel 1
+	nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+	nullptr // inertial sensor
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
+// create the chassis
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+	lateral_controller, // lateral PID settings
+	angular_controller, // angular PID settings
+	sensors // odometry sensors
+);
+
+// initialize function. Runs on program startup
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+pros::lcd::initialize(); // initialize brain screen
+chassis.calibrate(); // calibrate sensors
+// print position to brain screen
+pros::Task screen_task([&]() {
+while (true) {
+// print robot location to the brain screen
+pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+// delay to save resources
+pros::delay(20);
 }
-
+});
+}
 /**
  * Runs while the robot is in the disabled state of Field Management System or
  * the VEX Competition Switch, following either autonomous or opcontrol. When
@@ -73,23 +133,23 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+    // Set all motors to brake mode
+    left_motor_group.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+    right_motor_group.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+    
+    // loop forever
+    while (true) {
+        // get left y and right x positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
+        // move the robot
+        chassis.arcade(leftY, -rightX);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
+        // delay to save resources
+        pros::delay(25);
+    }
 }
- 
